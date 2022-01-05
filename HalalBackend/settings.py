31400f596676de9ce05a798f24a68c7ca6635aed
Motion Web LLC <85105629+Motion-web@users.noreply.github.com>
2022-01-05 14:13:21 +0600

    Update settings.py

--- HalalBackend/settings.py
+++ HalalBackend/settings.py
@@ -7,6 +7,8 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJECT_PATH = os.path.abspath(os.path.dirname(__name__))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
